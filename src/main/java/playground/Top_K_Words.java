c081dc85f601d7abc4a9d9bc0de413474ee211bf
sullis <github@seansullivan.com>
2024-06-10 13:37:40 -0700

    add Top K Words

---
+++ src/main/java/playground/Top_K_Words.java
@@ -0,0 +1,57 @@
+
+package playground;
+
+import java.util.*;
+import com.google.common.collect.Comparators;
+import java.util.stream.Collectors;
+
+/*
+ * Given a string of words, find the K most frequent words
+
+  Whitespace
+  Periods
+  Commas
+
+  Period or comma will be followed by a space
+
+
+   Edge cases:  
+      Upper case, Mixed case, Lower case words
+      All whitespace
+      All punctation
+      empty string
+
+ */
+
+public class Top_K_Words {
+  public static void main(String[] args) {
+
+    List<String> topK = process("Hello\n hello Hello aaa. bbb BBB, ", 2);
+    System.out.println(topK);
+  }
+
+  private static List<String> process(String input, int k) {
+    Map<String, Integer> map = new HashMap<>();
+    StringTokenizer tokenizer = new StringTokenizer(input, " \t\n,.");
+
+    while (tokenizer.hasMoreTokens()) {
+      String token = tokenizer.nextToken().toLowerCase();
+      Integer count = map.get(token);
+      if (count == null) {
+        count = 1;
+      } else {
+        count++;
+      }
+      map.put(token, count);
+    }
+
+    System.out.println(map);
+
+    return map.entrySet().stream()
+        .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+        .limit(k)
+        .map(e -> e.getKey())
+        .collect(Collectors.toList());
+
+  }
+}
